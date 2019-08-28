3d024cea4c859d919c301dab06f26797b17370be
jietian <jietian1992@qq.com>
2019-08-29 00:19:40 +0800

    first commit

---
+++ ly-gateway/src/main/java/cn/exihanfei/LyGateway.java
@@ -0,0 +1,14 @@
+package cn.exihanfei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class LyGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyGateway.class,args);
+    }
+}
